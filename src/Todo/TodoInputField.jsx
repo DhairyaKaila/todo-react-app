3f5a17a46133c4a964052f41424696793b91fd87
dhairykaila <dhairykaila@gmail.com>
2025-06-07 12:24:55 +0530

    update todo input field

--- src/Todo/TodoInputField.jsx
+++ src/Todo/TodoInputField.jsx
@@ -1,18 +1,25 @@
 import style from "./TodoInputField.module.css"
 
-const TodoInputField = ({addTask}) => {
+const TodoInputField = ({ addTask }) => {
 
     return <>
         <div className={style["todo-input-container"]}>
-            <input type="text" id="textInput" placeholder="Enter Todo Here"/>
+            <input type="text" id="textInput" placeholder="Enter Todo Here" />
             <input type="date" id="dateInput" />
             <button onClick={() => {
-                addTask(
-                document.getElementById("textInput").value.toString(),
-                document.getElementById("dateInput").value.toString()
-            );
-            document.getElementById("textInput").value = "";
-            document.getElementById("dateInput").value = "";
+                if (document.getElementById("textInput").value.toString().trim() != "" &&
+                    document.getElementById("dateInput").value.toString().trim() != "") {
+                    addTask(
+                        document.getElementById("textInput").value.toString(),
+                        document.getElementById("dateInput").value.toString()
+                    );
+                }
+                else{
+                    alert("plss enter value.");
+                }
+
+                document.getElementById("textInput").value = "";
+                document.getElementById("dateInput").value = "";
             }}>Add</button>
         </div>
     </>
